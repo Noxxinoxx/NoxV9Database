d8b91d570bbf0b71042d604a071382095d701646
Noa Andersson <86739524+Noxxinoxx@users.noreply.github.com>
2024-04-10 00:12:25 +0200

    dfg

--- NoxV9Database/src/database.rs
+++ NoxV9Database/src/database.rs
@@ -2,7 +2,6 @@ use serde_json::Value;
 use std::fs;
 use serde_json::Serializer; 
 use serde_json::Deserializer;
-#[derive(Serialize, Deserialize)]
 struct User {
     name : String, 
     email : String, 
