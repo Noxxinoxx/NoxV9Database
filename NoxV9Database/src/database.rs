1edb88700094302466ffb8894b74d0898385da5b
Noa Andersson <86739524+Noxxinoxx@users.noreply.github.com>
2024-04-10 00:10:52 +0200

    deleted user and changed it new standard.

--- NoxV9Database/src/database.rs
+++ NoxV9Database/src/database.rs
@@ -2,36 +2,27 @@ use serde_json::Value;
 use std::fs;
 use serde_json::Serializer; 
 use serde_json::Deserializer;
-#[derive(Serialize, Deserialize)]
+
+
+
+//mabye we want to change this to a socket connection insted of a standard http server.
+//use a TCP server for this.
 struct User {
     name : String, 
     email : String, 
     password : String,
 }
 
-pub fn read_database() {
-    let user_database = {
-        let file_content = fs::read_to_string("./Database/database.json").expect("LogRocket : error"); 
-        
+impl User {
 
-        let json = serde_json::from_str::<Value>(&file_content).expect("LogRocket : error in serializern");
-
-        let mut data : Vec<User> = Vec::new();
-
-        if let serde_json::Value::Array(items) = json {
-            for item in items {
-                println!("{}", item);
-
-                let p : User = 
-            }
+    pub fn new() -> User {
+        User {
+            name: "".to_string(),
+            email : "".to_string(),
+            password : "".to_string(),
         }
-        
-        
+    }    
 
-        
 
-    };
-
-    println!("{:?}", &user_database);
 
 }
