e24623c1e9b7ae4202b4a8bd0f3a6aaba3bf8383
Nox <nooaanderssondator@gmail.com>
2024-04-11 14:16:58 +0200

    databbase thing

---
+++ NoxV9Database/src/databasewriter.rs
@@ -0,0 +1,15 @@
+struct writer {
+    path : String
+}
+
+impl writer {
+    pub fn new() -> writer {
+        writer {
+            path : "C:/Users/Noxit/Desktop/NoxV9Database/NoxV9Database/Database/database.txt"
+        }
+    }
+
+    pub fn write_database(data: String) {
+        //create a string. 
+    }
+}
